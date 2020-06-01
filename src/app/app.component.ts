5fc510b660921bff58432ffe3c326d6bccc8b811
Maxim Mazurok <maxim@mazurok.com>
2020-06-01 23:56:23 +1000

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+// import 'gapi';
+// import 'gapi.auth2';
+// import 'gapi.client.calendar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-google-calendar-typescript-example';
+  isSignedIn = false;
+  pre = '';
+
+  initClient() {
+    const updateSigninStatus = this.updateSigninStatus.bind(this);
+    gapi.client
+      .init({
+        apiKey: 'AIzaSyAp_Insk0JjH4oxZ4I0-PLIydIno9jZEZ8',
+        clientId:
+          '507232289390-bbo3sbk9t7op0utpakhfc8juman0g025.apps.googleusercontent.com',
+        discoveryDocs: [
+          'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+        ],
+        scope: 'https://www.googleapis.com/auth/calendar.readonly',
+      })
+      .then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
+  }
+
+  updateSigninStatus(isSignedIn) {
+    this.isSignedIn = isSignedIn;
+    isSignedIn && this.listUpcomingEvents();
+  }
+
+  handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  handleSignoutClick() {
+    gapi.auth2.getAuthInstance().signOut();
+  }
+
+  listUpcomingEvents() {
+    const appendPre = this.appendPre.bind(this);
+    gapi.client.calendar.events
+      .list({
+        calendarId: 'primary',
+        timeMin: new Date().toISOString(),
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 10,
+        orderBy: 'startTime',
+      })
+      .then(function (response) {
+        var events = response.result.items;
+        appendPre('Upcoming events:');
+
+        if (events.length > 0) {
+          for (let i = 0; i < events.length; i++) {
+            var event = events[i];
+            var when = event.start.dateTime;
+            if (!when) {
+              when = event.start.date;
+            }
+            appendPre(event.summary + ' (' + when + ')');
+          }
+        } else {
+          appendPre('No upcoming events found.');
+        }
+      });
+  }
+
+  appendPre(text) {
+    this.pre += text + '\n';
+  }
+
+  loadGapi() {
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/api.js';
+    window.document.body.appendChild(script);
+    return new Promise<void>((resolve, reject) => {
+      script.addEventListener('error', (error) => reject(error));
+      script.addEventListener('load', () => resolve());
+    });
+  }
+
+  async ngOnInit() {
+    await this.loadGapi();
+    gapi.load('client:auth2', this.initClient.bind(this));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  
+}
